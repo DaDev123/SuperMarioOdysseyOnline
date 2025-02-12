d16167145b231bde1dd907cb32ea1bee802fa4f9
Secret Dev <131434750+DaDev123@users.noreply.github.com>
2025-02-12 11:17:55 +0100

    Add files via upload

--- include/al/util.hpp
+++ include/al/util.hpp
@@ -163,6 +163,12 @@ namespace al
 
     f32 calcDistance(al::LiveActor const *, al::LiveActor const*); // calculates distance between two actors
 
+    f32 calcDistanceV(al::LiveActor const *, al::LiveActor const*);
+
+    f32 calcDistanceH(al::LiveActor const *, al::LiveActor const*); 
+
+    f32 calcDistanceH(al::LiveActor const *, sead::Vector3f const&); 
+
     f32 calcDistance(al::LiveActor const *, sead::Vector3f const&); // calculates distance between an actor and a position in the world
 
     void calcFrontDir(sead::Vector3f *result, al::LiveActor const *actor);
