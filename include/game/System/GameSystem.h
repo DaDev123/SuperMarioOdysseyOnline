c05ea1557554a7dda97a2971af93f737f0539185
Secret Dev <131434750+DaDev123@users.noreply.github.com>
2025-02-12 11:07:35 +0100

    Add files via upload

--- include/game/System/GameSystem.h
+++ include/game/System/GameSystem.h
@@ -3,10 +3,18 @@
 #include "GameSystemInfo.h"
 #include "al/nerve/NerveExecutor.h"
 
+namespace al {
+    class Sequence;
+}
+
 class GameSystem : public al::NerveExecutor {
     public:
         void init();
-        void *gap;
+        al::Sequence* mSequence;
         al::GameSystemInfo* mSystemInfo;  // 0x18
         // 0x78 GameConfigData
 };
+
+namespace GameSystemFunction {
+    GameSystem* getGameSystem();
+}
